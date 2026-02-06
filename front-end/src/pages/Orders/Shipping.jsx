4225939c39dae89fadff0fd0d03d45c14d233f01
abbas <abbasrizk777@gmail.com>
2026-02-06 13:57:03 +0300

    Fix: Correcting Cart folder casing for Vercel build

--- front-end/src/pages/Orders/Shipping.jsx
+++ front-end/src/pages/Orders/Shipping.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import {
   saveShippingAddress,
   savePaymentMethod,
-} from "../../redux/features/cart/cartSlice";
+} from "../../redux/features/Cart/cartSlice";
 import ProgressSteps from "../../components/ProgressSteps";
 
 const Shipping = () => {
